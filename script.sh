ffa4678ddc318028ccb975c8a08cb889f98ce6fa
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-08-10 16:10:07 -0300

    Finished app

--- script.sh
+++ script.sh
@@ -11,3 +11,15 @@ echo '\n\n requesting with wrong body'
 curl --silent -X POST \
   --data-binary '{"invalid": "data"}' \
   localhost:3000/heroes
+
+echo '\n\n Creating Chapolin'
+CREATE=curl --silent -X POST \
+  --data-binary '{"name": "Chapolin", "age": "50", "power": "Super Strength"}' \
+  localhost:3000/heroes
+
+echo $CREATE 
+
+ID=$(echo $CREATE | jq.id)
+
+echo '\n\n requesting chapolin'
+curl localhost:3000/heroes/$id
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const http = require('http')
 const PORT = 3000
 const DEFAULT_HEADER = { "Content-Type": "application/json"}
+
 const HeroFactory = require('./factories/heroFactory')
 const heroService = HeroFactory.generateInstance()
+const Hero = require('./entities/hero')
 
 const routes = {
   '/heroes:get': async (request, response) => {
@@ -12,12 +14,49 @@ const routes = {
 
     return response.end()
   },
+  '/heroes:post': async (request, response) => {
+
+    //async iterator
+    for await (const data of request){
+      try {
+        const item = JSON.parse(data)
+        console.log('item', item)
+        const hero = new Hero(item)
+        const { error, valid } = hero.isValid()
+        if(!valid){
+          response.writeHead(400, DEFAULT_HEADER)
+          response.write(JSON.stringify({error: error.join(', ')}))
+          return response.end()
+        }
+
+        const id = await heroService.create(hero)
+        response.writeHead(201, DEFAULT_HEADER)
+        response.write(JSON.stringify({success: 'User created with success!', id}))
+
+        // Só jogamos o return aqui pois sabemos que é um objeto body por requisição
+        // se fosse um arquivo que sobe sob demanda ele poderia entrar mais vezes
+        // em um mesmo evento, ai removeriamos o return 
+        return response.end()
+      } catch (error) {
+          return handleError(response)(error)
+      }
+    }
+  },
   default: (request, response) => {
     response.write('Hello!')
     response.end()
   }
 }
 
+const handleError = response => {
+  return error => {
+    console.error("Deu Ruim! ***", error)
+    response.writeHead(500, DEFAULT_HEADER)
+    response.write(JSON.stringify({error: "Internal server error!!"}))
+
+    return response.end()
+  }
+}
 
 const handler = (request, response) => {
   const { url, method } = request
@@ -30,7 +69,7 @@ const handler = (request, response) => {
 
   const chosen = routes[key] || routes.default
 
-  return chosen(request, response)
+  return chosen(request, response).catch((handleError(response)))
 }
 
 
